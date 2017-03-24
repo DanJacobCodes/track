0d1c0314ee92a68b0de5a3dc7ceef8e2dc4d6a0e
Dan Jacob <dansamueljacob@gmail.com>
2017-03-24 11:11:46 -0700

    Created variables, began branching, downloaded images

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function){
+  $("#form").submit(function(event){
+
+    var os = $("#os").val();
+    var job = $("#job").val();
+    var business = $("#business").val();
+    var unicorn = $("#unicorn").val();
+    var tech = $("#tech").val();
+
+    if (os === "Mac" || "mac") {
+
+    }
+  }
+
+}
