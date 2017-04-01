3920491e21912337c22f7e6a3dcf4329ffa53259
Dan Jacob <dansamueljacob@gmail.com>
2017-04-01 15:12:19 -0400

    add else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("#form").submit(function(event) {
+$(document).ready(function(){
+  $("#track").submit(function(event) {
     event.preventDefault()
 
     var job = $("#job").val();
@@ -8,11 +8,22 @@ $(document).ready(function() {
     var os = $("#os").val();
     var start = $("#start").val();
 
-    if (job === "steady" && corp === "large" && tech === "micro" && os === "pc" && start === "no") {
-      $(".java", ".c", ".Ruby").hide();
+    if ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "no")){
+      $(".java", ".c", ".ruby").hide();
       $(".php").fadeIn();
-
-
+    }
+    else if ((job === "steady") && (corp === "large") && (tech === "goog") && (os === "mac") && (start === "yes")){
+      $(".php", ".c", ".ruby").hide();
+      $(".java").fadeIn();
+    }
+    else if((job === "contract") && (corp === "startup") && (tech === "goog") && (os === "mac") && (start === "yes")){
+      $(".java", ".c", ".php").hide();
+      $(".ruby").fadeIn();
+    }
+     else ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "yes")){
+      $(".java", ".php", ".ruby").hide();
+      $(".c").fadeIn();
+    }
 
     }
   });
