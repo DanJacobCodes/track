9dc2b76771298c840ecb2695dc744e167f2b2f22
Dan Jacob <dansamueljacob@gmail.com>
2017-04-01 16:09:59 -0400

    add descriptions of each language to all p elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#track").submit(function(event) {
+  $("#track").submit(function(event){
     event.preventDefault()
 
     var job = $("#job").val();
@@ -11,20 +11,16 @@ $(document).ready(function(){
     if ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "no")){
       $(".java", ".c", ".ruby").hide();
       $(".php").fadeIn();
-    }
-    else if ((job === "steady") && (corp === "large") && (tech === "goog") && (os === "mac") && (start === "yes")){
+    } else if ((job === "steady") && (corp === "large") && (tech === "goog") && (os === "mac") && (start === "yes")){
       $(".php", ".c", ".ruby").hide();
       $(".java").fadeIn();
-    }
-    else if((job === "contract") && (corp === "startup") && (tech === "goog") && (os === "mac") && (start === "yes")){
+    } else if ((job === "contract") && (corp === "startup") && (tech === "goog") && (os === "mac") && (start === "yes")){
       $(".java", ".c", ".php").hide();
       $(".ruby").fadeIn();
-    }
-     else ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "yes")){
+    } else {
+      ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "yes"))
       $(".java", ".php", ".ruby").hide();
       $(".c").fadeIn();
-    }
-
     }
   });
 });
