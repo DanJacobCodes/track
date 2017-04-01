575ea12968b05804401ed6eba08633774dcc77fb
Dan Jacob <dansamueljacob@gmail.com>
2017-04-01 16:44:24 -0400

    add proper indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,16 @@ $(document).ready(function(){
     if ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "no")){
       $(".java", ".c", ".ruby").hide();
       $(".php").fadeIn();
-    } else if ((job === "steady") && (corp === "large") && (tech === "goog") && (os === "mac") && (start === "yes")){
+    }
+    else if ((job === "steady") && (corp === "large") && (tech === "goog") && (os === "mac") && (start === "yes")){
       $(".php", ".c", ".ruby").hide();
       $(".java").fadeIn();
-    } else if ((job === "contract") && (corp === "startup") && (tech === "goog") && (os === "mac") && (start === "yes")){
+    }
+    else if ((job === "contract") && (corp === "startup") && (tech === "goog") && (os === "mac") && (start === "yes")){
       $(".java", ".c", ".php").hide();
       $(".ruby").fadeIn();
-    } else {
+    }
+    else {
       ((job === "steady") && (corp === "large") && (tech === "micro") && (os === "pc") && (start === "yes"))
       $(".java", ".php", ".ruby").hide();
       $(".c").fadeIn();
